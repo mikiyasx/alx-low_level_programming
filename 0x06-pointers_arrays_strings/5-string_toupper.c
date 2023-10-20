b0ba7f027b5ee0d2f2f100faca8f06f60815e4fa
mikiyasx <mikiistif219@gmail.com>
2023-10-19 20:06:03 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,9 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-	if (n[i] >= 'a' && n[i] <= 'z')
-	n[i] = n[i] - 32;
-	i++;
+		if (n[i] >= 'a' && n[i] <= 'z')
+		n[i] = n[i] - 32;
+		i++;
 	}
 	return (n);
 }
